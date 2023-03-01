98732b41323e5834e78b8ff72e1d0e15986c4780
Andrei Pokryshko <poxx@tut.by>
2023-03-01 14:31:27 +0300

    Seminar9, Homework/task66 - Done!

---
+++ Task66_Recurs_SumRange/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int RecSumRange(int from, int to)
+{
+    if (from==to) return to;
+    return from +RecSumRange(from+1,to);
+}
+
+int numM = GetNum("Введите М: ");
+int numN = GetNum("Введите N: ");
+Console.Write($"M = {numM}; N = {numN} -> {RecSumRange(numM, numN)}");
